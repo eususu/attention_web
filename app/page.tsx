37ae42c22da0a2df1306fd98449549f611bd1fef
lovian <belovian@gmail.com>
2024-05-25 19:29:08 +0900

    redirect to view.

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,6 @@
-import Tabs from "./components/Tabs";
-
-import ListAndDetail from "./components/ListAndDetail"
+import { redirect } from "next/navigation";
 
 
-async function load_qainfo_list(page:number) {
-  const service_name = 'kaai_poc'
-  const url=`http://172.30.1.15:8008/api/qa/${service_name}?page=${page}`
-
-  const response = await fetch(
-    url,
-    {
-      method: 'get',
-      mode: 'cors',
-      cache: 'no-cache',
-    }
-  );
-  console.log(response)
-  const body = await response.json()
-  console.log(body)
-
-  return body.qa_list
-}
-
-function onSelectItem(item:any) {
-  console.log(item)
-
-}
 export default async function Home() {
-  const qa_list = await load_qainfo_list(0)
-
-
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-full">
-        <Tabs></Tabs>
-      </div>
-      <ListAndDetail list={qa_list}/>
-      <div>
-        C
-      </div>
-    </main>
-  );
+  redirect('/view/0')
 }
